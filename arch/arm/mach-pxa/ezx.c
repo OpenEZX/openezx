eb455a5b28ec821b0c2bd527c490350073ba47a6
Daniel Ribeiro <drwyrm@gmail.com>
2008-10-31 17:38:07 -0200

    revert changes done on ezx/current

--- arch/arm/mach-pxa/ezx.c
+++ arch/arm/mach-pxa/ezx.c
@@ -832,11 +832,11 @@ static struct pxaohci_platform_data ezx_ohci_platform_data = {
 /* BP */
 #if defined(CONFIG_MACH_EZX_A780) || defined(CONFIG_MACH_EZX_E680)
 static struct ezxbp_config gen1_bp_data = {
-	.bp_reset = 82,
+	.bp_reset = 57,
 	.bp_wdi = 13,
 	.bp_wdi2 = 3,
 	.bp_rdy = 0,
-	.ap_rdy = 57,
+	.ap_rdy = 115,
 	.first_step = 2,
 };
 
