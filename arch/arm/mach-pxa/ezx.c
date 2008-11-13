627c9f5ede5b46db561b8c54cc2cd20721b81bca
Daniel Ribeiro <drwyrm@gmail.com>
2008-11-13 03:05:36 -0200

    remove .cs field from platform data, remove CS_INVERTED config flag, use gpio_to_irq

--- arch/arm/mach-pxa/ezx.c
+++ arch/arm/mach-pxa/ezx.c
@@ -669,21 +669,19 @@ static void ezx_pcap_init(void)
 }
 
 static struct pcap_platform_data ezx_pcap_platform_data = {
-	.cs     = 24,
-	.irq    = IRQ_GPIO1,
+	.irq    = gpio_to_irq(1),
 	.config = 0,
 	.init   = ezx_pcap_init,
 };
 
 static void pcap_cs_control(u32 command)
 {
-	if (command & PXA2XX_CS_ASSERT) {
-		gpio_set_value(ezx_pcap_platform_data.cs,
-		 (ezx_pcap_platform_data.config & PCAP_CS_INVERTED) ? 0 : 1);
-	} else {
-		gpio_set_value(ezx_pcap_platform_data.cs,
-		 (ezx_pcap_platform_data.config & PCAP_CS_INVERTED) ? 1 : 0);
-	}
+	if (command & PXA2XX_CS_ASSERT)
+		gpio_set_value(24,
+		 (machine_is_ezx_a780() || machine_is_ezx_e680()) ? 0 : 1);
+	else
+		gpio_set_value(24,
+		 (machine_is_ezx_a780() || machine_is_ezx_e680()) ? 1 : 0);
 }
 
 static struct pxa2xx_spi_chip ezx_pcap_chip_info = {
@@ -721,7 +719,7 @@ static void __init a780_init(void)
 
 	pxa_set_i2c_info(NULL);
 
-	ezx_pcap_platform_data.config |= PCAP_SECOND_PORT | PCAP_CS_INVERTED;
+	ezx_pcap_platform_data.config = PCAP_SECOND_PORT;
 	pxa2xx_set_spi_info(1, &ezx_spi_masterinfo);
 	spi_register_board_info(ARRAY_AND_SIZE(ezx_spi_boardinfo));
 
@@ -760,7 +758,7 @@ static void __init e680_init(void)
 	pxa_set_i2c_info(NULL);
 	i2c_register_board_info(0, ARRAY_AND_SIZE(e680_i2c_board_info));
 
-	ezx_pcap_platform_data.config |= PCAP_SECOND_PORT | PCAP_CS_INVERTED;
+	ezx_pcap_platform_data.config = PCAP_SECOND_PORT;
 	pxa2xx_set_spi_info(1, &ezx_spi_masterinfo);
 	spi_register_board_info(ARRAY_AND_SIZE(ezx_spi_boardinfo));
 
