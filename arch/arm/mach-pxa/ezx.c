e301edddb816fb03c05a427780813dcddb8051bc
Daniel Ribeiro <drwyrm@gmail.com>
2009-04-24 13:05:44 -0300

    move UP2OCR to udc_command so it is restaured when the phone resumes

--- arch/arm/mach-pxa/ezx.c
+++ arch/arm/mach-pxa/ezx.c
@@ -834,6 +834,8 @@ static void ezx_udc_command(int cmd)
 			tmp &= ~PCAP_BUSCTRL_USB_PU;
 		break;
 	case PXA2XX_UDC_CMD_CONNECT:
+		/* temp. set UP2OCR here until we have a transceiver driver */
+		UP2OCR = UP2OCR_SEOS(2);
 		if (machine_is_ezx_a780() || machine_is_ezx_e680())
 			tmp |= PCAP_BUSCTRL_USB_PU;
 		break;
@@ -1039,7 +1041,6 @@ static void __init a780_init(void)
 	pxa_set_mci_parent(&spi_pd->dev);
 	pxa_set_mci_info(&ezx_mci_platform_data);
 
-	UP2OCR = UP2OCR_SEOS(2);
 	pxa_set_udc_parent(&spi_pd->dev);
 	pxa_set_udc_info(&ezx_udc_info);
 
@@ -1158,7 +1159,6 @@ static void __init e680_init(void)
 	pxa_set_mci_parent(&spi_pd->dev);
 	pxa_set_mci_info(&ezx_mci_platform_data);
 
-	UP2OCR = UP2OCR_SEOS(2);
 	pxa_set_udc_parent(&spi_pd->dev);
 	pxa_set_udc_info(&ezx_udc_info);
 
@@ -1259,7 +1259,6 @@ static void __init a1200_init(void)
 	pxa_set_mci_parent(&spi_pd->dev);
 	pxa_set_mci_info(&ezx_mci_platform_data);
 
-	UP2OCR = UP2OCR_SEOS(2);
 	pxa_set_udc_parent(&spi_pd->dev);
 	pxa_set_udc_info(&ezx_udc_info);
 
@@ -1520,7 +1519,6 @@ static void __init a910_init(void)
 	platform_device_add(spi_pd);
 	spi_register_board_info(ARRAY_AND_SIZE(a910_spi_boardinfo));
 
-	UP2OCR = UP2OCR_SEOS(2);
 	pxa_set_udc_parent(&spi_pd->dev);
 	pxa_set_udc_info(&ezx_udc_info);
 
@@ -1619,7 +1617,6 @@ static void __init e6_init(void)
 	pxa_set_mci_parent(&spi_pd->dev);
 	pxa_set_mci_info(&ezx_mci_platform_data);
 
-	UP2OCR = UP2OCR_SEOS(2);
 	pxa_set_udc_parent(&spi_pd->dev);
 	pxa_set_udc_info(&ezx_udc_info);
 
@@ -1692,7 +1689,6 @@ static void __init e2_init(void)
 	pxa_set_mci_parent(&spi_pd->dev);
 	pxa_set_mci_info(&ezx_mci_platform_data);
 
-	UP2OCR = UP2OCR_SEOS(2);
 	pxa_set_udc_parent(&spi_pd->dev);
 	pxa_set_udc_info(&ezx_udc_info);
 
