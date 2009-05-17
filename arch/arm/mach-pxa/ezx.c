71be8847e5bca40144403e2daf07a3241f836147
Daniel Ribeiro <drwyrm@gmail.com>
2009-05-17 09:51:38 -0300

    remove leftover

--- arch/arm/mach-pxa/ezx.c
+++ arch/arm/mach-pxa/ezx.c
@@ -985,7 +985,6 @@ static struct i2c_board_info __initdata a780_i2c_board_info[] = {
 static struct platform_device *a780_devices[] __initdata = {
 	&a780_gpio_keys,
 	&gen1_flash_device,
-	&pcap_ts_device,
 };
 
 static void __init a780_init(void)
@@ -1095,7 +1094,6 @@ static struct i2c_board_info __initdata e680_i2c_board_info[] = {
 static struct platform_device *e680_devices[] __initdata = {
 	&e680_gpio_keys,
 	&gen1_flash_device,
-	&pcap_ts_device,
 };
 
 static void __init e680_init(void)
@@ -1203,7 +1201,6 @@ static struct i2c_board_info __initdata a1200_i2c_board_info[] = {
 static struct platform_device *a1200_devices[] __initdata = {
 	&a1200_gpio_keys,
 	&gen2_flash_device,
-	&pcap_ts_device,
 };
 
 static void __init a1200_init(void)
@@ -1533,7 +1530,6 @@ static struct i2c_board_info __initdata e6_i2c_board_info[] = {
 static struct platform_device *e6_devices[] __initdata = {
 	&e6_gpio_keys,
 	&gen2_flash_device,
-	&pcap_ts_device,
 };
 
 static void __init e6_init(void)
