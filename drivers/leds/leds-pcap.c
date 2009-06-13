e0c79bb05aafbaab5ef54f19ccf13357e949fb6f
Daniel Ribeiro <drwyrm@gmail.com>
2009-06-13 15:21:37 -0300

    update my copyright

--- drivers/leds/leds-pcap.c
+++ drivers/leds/leds-pcap.c
@@ -1,7 +1,7 @@
 /*
  * linux/drivers/leds/leds-pcap.c
  *
- * Copyright (C) 2008 Daniel Ribeiro <drwyrm@gmail.com>
+ * Copyright (C) 2009 Daniel Ribeiro <drwyrm@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
