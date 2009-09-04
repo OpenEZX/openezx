0b1833bec2c4486607178a752854ae6aead023f2
Ilya Petrov <ilya.muromec@gmail.com>
2009-09-04 22:34:52 +0300

    set eoc data to mfd

--- arch/arm/mach-pxa/ezx.c
+++ arch/arm/mach-pxa/ezx.c
@@ -23,6 +23,7 @@
 #include <linux/gpio.h>
 #include <linux/spi/spi.h>
 #include <linux/mfd/ezx-pcap.h>
+#include <linux/mfd/ezx-eoc.h>
 #include <linux/spi/mmc_spi.h>
 #include <linux/irq.h>
 #include <linux/leds.h>
@@ -310,6 +311,10 @@ static unsigned long gen2_pin_config[] __initdata = {
 	GPIO17_GPIO,				/* CAM_FLASH */
 };
 #endif
+
+static struct eoc_platform_data eoc_platform_data = {
+	.irq_base	= IRQ_BOARD_START + PCAP_NIRQS,
+};
 
 #ifdef CONFIG_MACH_EZX_A780
 static unsigned long a780_pin_config[] __initdata = {
@@ -985,10 +990,6 @@ void ezx_mach_switch_mode(enum eoc_transceiver_mode mode)
 	}
 }
 
-struct eoc_platform_data ezx_eoc_data = {
-	.mach_switch_mode = ezx_mach_switch_mode,
-};
-
 #endif
 
 #ifdef CONFIG_MACH_EZX_A780
@@ -1429,7 +1430,7 @@ static struct i2c_board_info __initdata a1200_i2c_board_info[] = {
 		I2C_BOARD_INFO("tea5767", 0x81),
 	}, {
 		I2C_BOARD_INFO("ezx-eoc", 0x17),
-		.platform_data = &ezx_eoc_data,
+		.platform_data = &eoc_platform_data,
 	},
 };
 
@@ -1687,7 +1688,7 @@ static struct i2c_board_info __initdata a910_i2c_board_info[] = {
 		.platform_data = &a910_lp3944_leds,
 	}, {
 		I2C_BOARD_INFO("ezx-eoc", 0x17),
-		.platform_data = &ezx_eoc_data,
+		.platform_data = &eoc_platform_data,
 	},
 };
 
@@ -1817,7 +1818,7 @@ static struct i2c_board_info __initdata e6_i2c_board_info[] = {
 		I2C_BOARD_INFO("tea5767", 0x81),
 	}, {
 		I2C_BOARD_INFO("ezx-eoc", 0x17),
-		.platform_data = &ezx_eoc_data,
+		.platform_data = &eoc_platform_data,
 	},
 };
 
@@ -1917,8 +1918,9 @@ static struct spi_board_info e2_spi_boardinfo[] __initdata = {
 static struct i2c_board_info __initdata e2_i2c_board_info[] = {
 	{
 		I2C_BOARD_INFO("tea5767", 0x81),
+        }, {
 		I2C_BOARD_INFO("ezx-eoc", 0x17),
-		.platform_data = &ezx_eoc_data,
+		.platform_data = &eoc_platform_data,
 	},
 };
 
