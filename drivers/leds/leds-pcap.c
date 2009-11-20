77dbd0f8d528562a11b4071526f9e44abc0175e7
Antonio Ospite <ospite@studenti.unina.it>
2009-11-20 15:18:18 +0100

    Cosmetic cleanup.
    Plus:
     - Add a warning if the led type is unknown
     - don't return anything in pcap_led_exit, as its return value is void

--- drivers/leds/leds-pcap.c
+++ drivers/leds/leds-pcap.c
@@ -1,5 +1,5 @@
 /*
- * linux/drivers/leds/leds-pcap.c
+ * leds-pcap.c - leds driver for PCAP mfd chip used on EZX platform
  *
  * Copyright (C) 2009 Daniel Ribeiro <drwyrm@gmail.com>
  *
@@ -8,9 +8,7 @@
  * published by the Free Software Foundation.
  */
 
-#include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/device.h>
+#include <linux/module.h>
 #include <linux/workqueue.h>
 #include <linux/leds.h>
 #include <linux/leds-pcap.h>
@@ -65,8 +63,10 @@ static void pcap_led_work(struct work_struct *work)
 		ezx_pcap_write(led->pcap, PCAP_REG_PERIPH, tmp);
 		return;
 	default:
+		dev_warn(led->ldev.dev, "unknown led type %d\n", led->type);
 		return;
 	}
+
 	/* turn off */
 	tmp &= ~(e | (PCAP_LED_T_MASK << t) | (PCAP_LED_C_MASK << c));
 
@@ -90,11 +90,13 @@ static int __devinit pcap_led_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "%s: no platform data\n", __func__);
 		return -EINVAL;
 	}
+
 	for (i = 0; i < pdata->num_leds; i++) {
 		struct pcap_led *led = &pdata->leds[i];
 		led->ldev.name = led->name;
 		led->ldev.brightness_set = pcap_led_set_brightness;
 		led->pcap = dev_get_drvdata(pdev->dev.parent);
+
 		if (led->gpio & PCAP_LED_GPIO_EN) {
 			int gpio = (led->gpio & PCAP_LED_GPIO_VAL_MASK);
 			err = gpio_request(gpio, "PCAP LED");
@@ -106,14 +108,17 @@ static int __devinit pcap_led_probe(struct platform_device *pdev)
 			gpio_direction_output(gpio,
 				(led->gpio & PCAP_LED_GPIO_INVERT) ? 1 : 0);
 		}
+
 		err = led_classdev_register(&pdev->dev, &led->ldev);
 		if (err) {
 			dev_err(&pdev->dev, "couldn't register LED %s\n",
 					led->name);
 			goto fail;
 		}
+
 		INIT_WORK(&led->work, pcap_led_work);
 	}
+
 	return 0;
 
 fail:
@@ -121,6 +126,7 @@ fail:
 		led_classdev_unregister(&pdata->leds[--i].ldev);
 		cancel_work_sync(&pdata->leds[i].work);
 	}
+
 	return err;
 }
 
@@ -133,6 +139,7 @@ static int __devexit pcap_led_remove(struct platform_device *pdev)
 		led_classdev_unregister(&pdata->leds[i].ldev);
 		cancel_work_sync(&pdata->leds[i].work);
 	}
+
 	return 0;
 }
 
@@ -140,7 +147,7 @@ static struct platform_driver pcap_led_driver = {
 	.probe  = pcap_led_probe,
 	.remove = __devexit_p(pcap_led_remove),
 	.driver = {
-		.name = "pcap-leds",
+		.name   = "pcap-leds",
 		.owner	= THIS_MODULE,
 	},
 };
@@ -152,7 +159,7 @@ static int __init pcap_led_init(void)
 
 static void __exit pcap_led_exit(void)
 {
-	return platform_driver_unregister(&pcap_led_driver);
+	platform_driver_unregister(&pcap_led_driver);
 }
 
 module_init(pcap_led_init);
